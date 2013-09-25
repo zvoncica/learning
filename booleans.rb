9ea581f5e8e4f35316f6b5b7d3c18c1ce7525916
Andrei Serdeliuc <andrei@serdeliuc.ro>
2013-09-25 22:36:25 +0100

    convert tabs to spaces

--- booleans.rb
+++ booleans.rb
@@ -3,41 +3,41 @@
 name = nil
 
 if name == "andrei" then
-	puts "it is andrei"
+    puts "it is andrei"
 else
-	puts "it is not andrei"
+    puts "it is not andrei"
 end
 
 p name == "andrei"
 
 if name then 
-	puts "yes"
+    puts "yes"
 else
-	puts "no"
+    puts "no"
 end
 
 name2 = "andrei"
 
 if name2 == "tea" then
-	puts "hello #{name2}"
+    puts "hello #{name2}"
 elsif name2 == "andrei"
-	puts "hey #{name2}"
+    puts "hey #{name2}"
 elsif name2 == "jon"
-	puts "hi #{name2}"
+    puts "hi #{name2}"
 else 
-	puts "bye #{name2}"
+    puts "bye #{name2}"
 end
 
 
 case name2
 when "tea" 
-	puts "hello #{name2}"
+    puts "hello #{name2}"
 when "andrei"
-	puts "hey #{name2}"
+    puts "hey #{name2}"
 when "jon"
-	puts "hi #{name2}"
+    puts "hi #{name2}"
 else         
-	puts "bye #{name2}"
+    puts "bye #{name2}"
 end
 
 puts "happy fork" if name2 == "tea"
